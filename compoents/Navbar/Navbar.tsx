4ea8048b7364f54e18fadda1b80403cb19ba378d
Bdbmzwsc <88311279+Bdbmzwsc@users.noreply.github.com>
2022-09-03 10:38:57 +0800

    added mobile adpation

--- compoents/Navbar/Navbar.tsx
+++ compoents/Navbar/Navbar.tsx
@@ -1,13 +1,36 @@
 import styles from './Navbar.module.css';
+import React from 'react';
 interface indnaConfig {
     ind: string,
     nam: string
 }
 export default function Navbar({title,config}: any){
-    return(<div className={styles.Navbar}>
+    const [iscli,Setiscli]=React.useState(false);
+    return(
+    <div className={styles.Navbar}>
+        <button className={styles.navbarto}
+        onClick={() => Setiscli(!iscli)}
+        >
+123
+        </button>
         <div className={styles.title}>{title}</div>
+        <div className={styles.navbarhi}>
         {config.map((nameindex: indnaConfig)=>{
-            return <div className={styles.indexbtn}><a href={nameindex.ind}>{nameindex.nam}</a></div>
+            return <div className={styles.indexbtn}>
+                <a href={nameindex.ind}>{nameindex.nam}</a>
+                </div>
         })}
+        </div>
+        {iscli && (
+            <div className={`${styles.navbarmo} ${!iscli && styles.navbarmohi}`}>
+                     {config.map((nameindex: indnaConfig)=>{
+            return <div className={styles.indexbtn}>
+                <a onClick={()=>Setiscli(false)} href={nameindex.ind}>{nameindex.nam}</a>
+                </div>
+        })}
+            </div>
+        )
+
+        }
     </div>)
 }
